522193da4bf392d8d80aed5506d63bc77334fc78
Konstantin Baranov <kbaranov@mobiledep.ru>
2023-10-21 10:17:24 +0300

    arrays/2.cpp: initialization.

--- arrays/2.cpp
+++ arrays/2.cpp
@@ -12,7 +12,7 @@
  */
 void insertionSort(int* arr, int n) {
     for(int i{1}; i < n; ++i) {
-        int key = arr[i];
+        int key{ arr[i] };
         int j{ i - 1 };
 
         // Move elements of arr[0..i-1], that are
